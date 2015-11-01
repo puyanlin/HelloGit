3d0af4753c605763a831655707ed3d68db296322
Joseph Lin <puyan.tw@gmail.com>
2015-11-01 23:33:02 +0800

    add 2 activity

--- app/src/main/java/tw/puyan/hellogit/BlackyActivity.java
+++ app/src/main/java/tw/puyan/hellogit/BlackyActivity.java
@@ -1,5 +1,6 @@
 package tw.puyan.hellogit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class BlackyActivity extends AppCompatActivity {
+public class BlackyActivity extends AppCompatActivity  implements  View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,25 @@ public class BlackyActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button btnJohn = (Button) findViewById(R.id.btnJohn);
+        btnJohn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent johnIntent = new Intent(BlackyActivity.this,JohnActivity.class);
+                startActivity(johnIntent);
+            }
+        });
+
+        Button btnShanika = (Button) findViewById(R.id.btnShanika);
+        btnShanika.setOnClickListener(this);
+    }
+
+    @Override
+    public  void  onClick(View v){
+        Intent shanikaIntent = new Intent(BlackyActivity.this,ShanikaActivity.class);
+        startActivity(shanikaIntent);
     }
 
     @Override
