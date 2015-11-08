ac112cbd6a77a3ee194c49888d67d2e83bd4fe34
Joseph Lin <puyan.tw@gmail.com>
2015-11-08 20:07:23 +0800

    add view widget to john activity
    learning Android!

--- app/src/main/java/tw/puyan/hellogit/JohnActivity.java
+++ app/src/main/java/tw/puyan/hellogit/JohnActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class JohnActivity extends AppCompatActivity {
 
@@ -22,6 +24,22 @@ public class JohnActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        //declare a local field to catch find out view(button) object
+        Button btn = (Button) findViewById(R.id.btn);
+
+        //then, set clicked listener to handle click event
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //handling body
+
+
+                //get TextView
+                TextView tv = (TextView) findViewById(R.id.tv);
+                tv.setText("HelloTaiwan");
             }
         });
     }
